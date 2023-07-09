9e0e8ddaa38e29282afc2d407025ca41e6cd701e
mustafa-altai77 <116103164+mustafa-altai77@users.noreply.github.com>
2023-07-09 17:18:52 +0400

    Update ApiClient.java

--- app/src/main/java/com/example/discaount/data/network/ApiClient.java
+++ app/src/main/java/com/example/discaount/data/network/ApiClient.java
@@ -7,7 +7,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "https://shayoub.com/discount/public/api/";
+  
   private ApiInterface apiInterface;
   private static ApiClient INSTANCE;
   public static Retrofit retrofit;
